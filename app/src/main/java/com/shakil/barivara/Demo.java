c204e7771096dfdfeea0c882ab8dc7d9b550b747
shakiz <shakil.py@gmail.com>
2021-04-03 23:48:26 +0600

    all setup and base test done

---
+++ app/src/main/java/com/shakil/barivara/Demo.java
@@ -0,0 +1,31 @@
+package com.shakil.barivara;
+
+public class Demo {
+    private String Data1;
+    private String Data2;
+    private String Data3;
+
+    public String getData1() {
+        return Data1;
+    }
+
+    public void setData1(String data1) {
+        Data1 = data1;
+    }
+
+    public String getData2() {
+        return Data2;
+    }
+
+    public void setData2(String data2) {
+        Data2 = data2;
+    }
+
+    public String getData3() {
+        return Data3;
+    }
+
+    public void setData3(String data3) {
+        Data3 = data3;
+    }
+}
