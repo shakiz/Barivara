ab902132782c10caaf528492d9f9d746d9a07595
shakiz <shakil.py@gmail.com>
2021-04-03 23:49:28 +0600

    refactored activity name

--- app/src/main/java/com/shakil/barivara/DemoActivity.java
+++ app/src/main/java/com/shakil/barivara/DemoActivity.java
@@ -9,20 +9,17 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
-public class MainActivity extends AppCompatActivity {
+public class DemoActivity extends AppCompatActivity {
     EditText Data1, Data2, Data3;
     Button Submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_demo);
         FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
         DatabaseReference databaseReference = firebaseDatabase.getReference("demo");
 
