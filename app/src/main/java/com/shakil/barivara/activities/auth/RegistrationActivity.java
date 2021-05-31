6247ecbbbe3d19b28f4d611905165d533a48e330
shakiz <shakil.py@gmail.com>
2021-06-01 01:24:59 +0600

    login and registration activity added

---
+++ app/src/main/java/com/shakil/barivara/activities/auth/RegistrationActivity.java
@@ -0,0 +1,16 @@
+package com.shakil.barivara.activities.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.shakil.barivara.R;
+
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
