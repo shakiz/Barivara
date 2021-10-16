c6f83577e1694ead20d93b0e7f65e9e54059acbe
shakiz <shakil.py@gmail.com>
2021-10-16 22:41:17 +0600

    removed unnecessary mobile registration

--- app/src/main/java/com/shakil/barivara/activities/auth/RegistrationActivity.java
+++ app/src/main/java/com/shakil/barivara/activities/auth/RegistrationActivity.java
@@ -8,7 +8,6 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
 import androidx.databinding.DataBindingUtil;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -30,7 +29,6 @@ public class RegistrationActivity extends AppCompatActivity {
     private FirebaseAuth firebaseAuth;
     private UX ux;
     private Tools tools;
-    private String registerWithStr;
     private Validation validation;
     private final Map<String, String[]> hashMap = new HashMap();
 
@@ -41,7 +39,6 @@ public class RegistrationActivity extends AppCompatActivity {
 
         //region init UI
         initUI();
-        registerWithStr = getString(R.string.mobile);
         //endregion
 
         //region bind UI with components
@@ -61,8 +58,8 @@ public class RegistrationActivity extends AppCompatActivity {
     //region bind UI with components
     private void bindUiWithComponents() {
         //region validation
-        validation(new String[]{"mobileNumber"},
-                new String[]{getString(R.string.mobile_validation)});
+        validation.setEditTextIsNotEmpty(new String[]{"email", "password"},
+                new String[]{getString(R.string.email_validation), getString(R.string.password_validation)});
         //endregion
 
         //region login click listener
@@ -74,30 +71,6 @@ public class RegistrationActivity extends AppCompatActivity {
         });
         //endregion
 
-        //region register with click
-        activityBinding.emailIdLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                registerWithStr = getString(R.string.email);
-                registerWith(registerWithStr);
-                //region validation
-                validation(new String[]{"email", "password"},
-                        new String[]{getString(R.string.email_validation), getString(R.string.password_validation)});
-                //endregion
-            }
-        });
-        activityBinding.mobileLayout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                registerWithStr = getString(R.string.mobile);
-                registerWith(registerWithStr);
-                //region validation
-                validation(new String[]{"mobileNumber"},
-                        new String[]{getString(R.string.mobile_validation)});
-                //endregion
-            }
-        });
-        //endregion
 
         //region register click listener
         activityBinding.signUp.setOnClickListener(new View.OnClickListener() {
@@ -105,73 +78,25 @@ public class RegistrationActivity extends AppCompatActivity {
             public void onClick(View v) {
                 if (validation.isValid()) {
                     if (tools.hasConnection()) {
-                        //region check for register with mobile or email
-                        if (registerWithStr.equals(getString(R.string.email))) {
-                            //region check email address validation
-                            if (tools.validateEmailAddress(activityBinding.email.getText().toString())) {
-                                if (activityBinding.password.getText().toString().length() >= 6) {
-                                    registerWithEmailPass();
-                                } else {
-                                    Toast.makeText(RegistrationActivity.this, getString(R.string.password_must_be_six_character), Toast.LENGTH_SHORT).show();
-                                }
+                        //region check email address validation
+                        if (tools.validateEmailAddress(activityBinding.email.getText().toString())) {
+                            if (activityBinding.password.getText().toString().length() >= 6) {
+                                registerWithEmailPass();
                             } else {
-                                Toast.makeText(RegistrationActivity.this, getString(R.string.not_a_valid_email_address),
-                                        Toast.LENGTH_SHORT).show();
+                                Toast.makeText(RegistrationActivity.this, getString(R.string.password_must_be_six_character), Toast.LENGTH_LONG).show();
                             }
-                            //endregion
-                        } else if (registerWithStr.equals(getString(R.string.mobile))) {
-                            //region check for mobile number validation and do register
-                            if (tools.isValidMobile(activityBinding.mobileNumber.getText().toString())){
-                                registerWithMobile();
-                            }
-                            else{
-                                Toast.makeText(RegistrationActivity.this, getString(R.string.not_a_valid_mobile_number),
-                                        Toast.LENGTH_SHORT).show();
-                            }
-                            //endregion
-                        }
-                        else{
-                            Toast.makeText(RegistrationActivity.this, getString(R.string.validation_registration_type),
-                                    Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(RegistrationActivity.this, getString(R.string.not_a_valid_email_address),
+                                    Toast.LENGTH_LONG).show();
                         }
                         //endregion
                     }
                 } else {
-                    Toast.makeText(RegistrationActivity.this, getString(R.string.no_internet_title), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(RegistrationActivity.this, getString(R.string.no_internet_title), Toast.LENGTH_LONG).show();
                 }
             }
         });
         //endregion
-    }
-    //endregion
-
-    //region change register with UI
-    private void registerWith(String registerWith) {
-        if (registerWith.equals(getString(R.string.email))) {
-            activityBinding.mainMobileRegistrationLayout.setVisibility(View.GONE);
-            activityBinding.mainEmailRegistrationLayout.setVisibility(View.VISIBLE);
-            activityBinding.emailIdLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.rectangle_background_filled_gender));
-            activityBinding.EmailId.setTextColor(ContextCompat.getColor(this, R.color.md_white_1000));
-            activityBinding.mobileLayout.setBackgroundResource(0);
-            activityBinding.Mobile.setTextColor(ContextCompat.getColor(this, R.color.md_green_800));
-            activityBinding.MobileIcon.setImageResource(R.drawable.ic_call_green);
-            activityBinding.EmailIcon.setImageResource(R.drawable.ic_email_white);
-        } else {
-            activityBinding.mainEmailRegistrationLayout.setVisibility(View.GONE);
-            activityBinding.mainMobileRegistrationLayout.setVisibility(View.VISIBLE);
-            activityBinding.mobileLayout.setBackground(ContextCompat.getDrawable(this, R.drawable.rectangle_background_filled_gender));
-            activityBinding.emailIdLayout.setBackgroundResource(0);
-            activityBinding.EmailId.setTextColor(ContextCompat.getColor(this, R.color.md_green_800));
-            activityBinding.Mobile.setTextColor(ContextCompat.getColor(this, R.color.md_white_1000));
-            activityBinding.MobileIcon.setImageResource(R.drawable.ic_call_white);
-            activityBinding.EmailIcon.setImageResource(R.drawable.ic_email_green);
-        }
-    }
-    //endregion
-
-    //region custom validation
-    private void validation(String[] resNames, String[] validationMessages) {
-        validation.setEditTextIsNotEmpty(resNames, validationMessages);
     }
     //endregion
 
@@ -199,14 +124,6 @@ public class RegistrationActivity extends AppCompatActivity {
                 ux.removeLoadingView();
             }
         });
-    }
-    //endregion
-
-    //region register with mobile number
-    private void registerWithMobile(){
-        Intent intent = new Intent(RegistrationActivity.this, MobileRegVerificationActivity.class);
-        intent.putExtra("mobile", activityBinding.mobileNumber.getText().toString());
-        startActivity(intent);
     }
     //endregion
 
