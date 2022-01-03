f72c186720adf64d74fa6daf20535dd82cee467f
shakiz <shakil.py@gmail.com>
2022-01-04 00:18:08 +0600

    profile page part 2

--- app/src/main/java/com/shakil/barivara/activities/profile/ProfileActivity.java
+++ app/src/main/java/com/shakil/barivara/activities/profile/ProfileActivity.java
@@ -1,6 +1,7 @@
 package com.shakil.barivara.activities.profile;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,23 +9,16 @@ import android.view.View;
 
 import com.shakil.barivara.R;
 import com.shakil.barivara.activities.onboard.MainActivity;
+import com.shakil.barivara.databinding.ActivityProfileBinding;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private ActivityProfileBinding activityBinding;
+    private boolean isEditable = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_profile);
-
-        setSupportActionBar(activitySettingsBinding.toolBar);
-
-        activitySettingsBinding.toolBar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                onBackPressed();
-            }
-        });
+        activityBinding = DataBindingUtil.setContentView(this, R.layout.activity_profile);
 
         //region init objects and perform all UI operations
         initUI();
@@ -40,7 +34,36 @@ public class ProfileActivity extends AppCompatActivity {
 
     //region perform all UI operations
     private void bindUiWithComponents() {
+        activityBinding.backToHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
 
+        activityBinding.editIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activityBinding.Name.setFocusable(true);
+                activityBinding.Mobile.setFocusable(true);
+                activityBinding.Email.setFocusable(true);
+                activityBinding.Address.setFocusable(true);
+                activityBinding.DOB.setFocusable(true);
+                activityBinding.editIcon.setVisibility(View.GONE);
+            }
+        });
+
+        activityBinding.cancelEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                activityBinding.editIcon.setVisibility(View.VISIBLE);
+                activityBinding.Name.setFocusable(false);
+                activityBinding.Mobile.setFocusable(false);
+                activityBinding.Email.setFocusable(false);
+                activityBinding.Address.setFocusable(false);
+                activityBinding.DOB.setFocusable(false);
+            }
+        });
     }
     //endregion
 
