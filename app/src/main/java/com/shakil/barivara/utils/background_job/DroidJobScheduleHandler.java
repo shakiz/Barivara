0f10e4b72f74055aba8a38ee6c808ef8ee31de5a
shakiz <activities335@gmail.com>
2022-04-02 19:59:37 +0600

    removed all background service codes

--- app/src/main/java/com/shakil/barivara/utils/background_job/DroidJobScheduleHandler.java
+++
@@ -1,9 +0,0 @@
-package com.shakil.barivara.utils.background_job;
-
-import android.content.Context;
-
-public interface DroidJobScheduleHandler {
-
-    void onJobSchedulerFire(Context context, String jobName);
-
-}
