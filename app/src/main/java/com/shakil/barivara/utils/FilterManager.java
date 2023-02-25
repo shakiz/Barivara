db8deedd7ea0d875492e26a58534141b46970e94
shakiz <activities335@gmail.com>
2023-02-25 21:17:30 +0600

    Update filter callbacks

--- app/src/main/java/com/shakil/barivara/utils/FilterManager.java
+++ app/src/main/java/com/shakil/barivara/utils/FilterManager.java
@@ -5,15 +5,10 @@ import com.shakil.barivara.model.meter.Meter;
 import com.shakil.barivara.model.room.Rent;
 import com.shakil.barivara.model.room.Room;
 import com.shakil.barivara.model.tenant.Tenant;
-
 import java.util.ArrayList;
 
 public class FilterManager{
 
-    //region room search listener
-    public interface onFilterClick{
-        void onClick(ArrayList<Room> objects);
-    }
     public void onFilterClick(String searchText, ArrayList<Room> sourceList, onFilterClick onFilterClick){
         ArrayList<Room> roomList = new ArrayList<>();
         for (int start = 0; start < sourceList.size(); start++) {
@@ -25,12 +20,7 @@ public class FilterManager{
             onFilterClick.onClick(roomList);
         }
     }
-    //endregion
 
-    //region rent search listener
-    public interface onRentFilterClick{
-        void onClick(ArrayList<Rent> objects);
-    }
     public void onFilterClick(String searchText, ArrayList<Rent> sourceList, onRentFilterClick onRentFilterClick){
         ArrayList<Rent> rentList = new ArrayList<>();
         for (int start = 0; start < sourceList.size(); start++) {
@@ -42,12 +32,7 @@ public class FilterManager{
             onRentFilterClick.onClick(rentList);
         }
     }
-    //endregion
 
-    //region meter search listener
-    public interface onMeterFilterClick{
-        void onClick(ArrayList<Meter> objects);
-    }
     public void onFilterClick(String searchText, ArrayList<Meter> sourceList, onMeterFilterClick onMeterFilterClick){
         ArrayList<Meter> meterList = new ArrayList<>();
         for (int start = 0; start < sourceList.size(); start++) {
@@ -59,12 +44,7 @@ public class FilterManager{
             onMeterFilterClick.onClick(meterList);
         }
     }
-    //endregion
 
-    //region electricity bill search listener
-    public interface onBillFilterClick{
-        void onClick(ArrayList<ElectricityBill> objects);
-    }
     public void onFilterClick(String searchText, ArrayList<ElectricityBill> sourceList, onBillFilterClick onBillFilterClick){
         ArrayList<ElectricityBill> billList = new ArrayList<>();
         for (int start = 0; start < sourceList.size(); start++) {
@@ -76,12 +56,7 @@ public class FilterManager{
             onBillFilterClick.onClick(billList);
         }
     }
-    //endregion
 
-    //region tenant search listener
-    public interface onTenantFilterClick{
-        void onClick(ArrayList<Tenant> objects);
-    }
     public void onFilterClick(String searchText, ArrayList<Tenant> sourceList, onTenantFilterClick onTenantFilterClick){
         ArrayList<Tenant> billList = new ArrayList<>();
         for (int start = 0; start < sourceList.size(); start++) {
@@ -93,5 +68,11 @@ public class FilterManager{
             onTenantFilterClick.onClick(billList);
         }
     }
-    //endregion
+
+    public interface onRentFilterClick{ void onClick(ArrayList<Rent> objects);}
+    public interface onFilterClick{ void onClick(ArrayList<Room> objects);}
+    public interface onMeterFilterClick{ void onClick(ArrayList<Meter> objects);}
+    public interface onTenantFilterClick{ void onClick(ArrayList<Tenant> objects);}
+    public interface onBillFilterClick{ void onClick(ArrayList<ElectricityBill> objects);}
+
 }
