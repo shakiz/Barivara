6ee327c8aa4f04af0253d81939f6f52f8b3633c9
shakiz <activities335@gmail.com>
2023-02-25 21:08:08 +0600

    Remove unused code from UX class

--- app/src/main/java/com/shakil/barivara/utils/UX.java
+++ app/src/main/java/com/shakil/barivara/utils/UX.java
@@ -1,55 +1,19 @@
 package com.shakil.barivara.utils;
 
-import android.app.Activity;
-import android.app.DatePickerDialog;
 import android.app.Dialog;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.icu.text.DateFormat;
-import android.icu.text.SimpleDateFormat;
-import android.os.Build;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.CheckBox;
-import android.widget.CompoundButton;
-import android.widget.DatePicker;
-import android.widget.EditText;
-import android.widget.Spinner;
-import android.widget.TextView;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 import com.shakil.barivara.R;
 
-import java.util.Calendar;
-import java.util.Locale;
-
 public class UX {
-    private final Context context;
     public Dialog loadingDialog;
 
     public UX(Context context) {
-        this.context = context;
         loadingDialog = new Dialog(context);
     }
 
-    //Toolbar
-    public void setToolbar(Toolbar toolbar, final Activity from, final Class to){
-        ((AppCompatActivity) context).setSupportActionBar(toolbar);
-        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                context.startActivity(new Intent(from, to));
-            }
-        });
-//        ((AppCompatActivity) context).getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_left_arrow);
-    }
-    //End toolbar
-
-    //Loading view creator and cancel
     public void getLoadingView(){
         loadingDialog.setContentView(R.layout.loading_layout);
         loadingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
@@ -60,80 +24,4 @@ public class UX {
     public void removeLoadingView(){
         if (loadingDialog.isShowing()) loadingDialog.cancel();
     }
-    //End loading view
-
-    //Clear UI components
-    public void clearDetailsUI(int[] resIds, View view){
-        for (int resId : resIds){
-            View child = view.findViewById(resId);
-            if (child instanceof TextView){
-                TextView textView = (TextView) child;
-                textView.setText("");
-            }
-            else if(child instanceof EditText){
-                EditText editText = (EditText) child;
-                editText.setText("");
-            }
-        }
-    }
-    //End
-
-    //Spinner on change
-    public void onSpinnerChange(Spinner spinner, onSpinnerChangeListener listener) {
-        final onSpinnerChangeListener customListener = listener;
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                customListener.onChange(parent, view, position, id);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-    }
-
-    public interface onSpinnerChangeListener {
-        void onChange(AdapterView<?> parent, View view, int position, long id);
-    }
-    //End spinner
-
-    public void onChange(CheckBox checkBox, final onChangeListener listener) {
-        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (listener != null) {
-                    listener.onChange(isChecked);
-                }
-            }
-        });
-    }
-
-    public interface onChangeListener {
-        void onChange(boolean isChecked);
-    }
-
-    //Date on click operation
-    public void getAndSetDate(final TextView dateViewTXT){
-        DateFormat dateFormatter = null;
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-            dateFormatter = new SimpleDateFormat("dd-MMM-yyyy", Locale.US);
-        }
-        Calendar newCalendar = Calendar.getInstance();
-        final DateFormat finalDateFormatter = dateFormatter;
-        DatePickerDialog datePickerDialog = new DatePickerDialog(context, new DatePickerDialog.OnDateSetListener() {
-
-            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
-                Calendar newDate = Calendar.getInstance();
-                newDate.set(year, monthOfYear, dayOfMonth);
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                    dateViewTXT.setText(finalDateFormatter.format(newDate.getTime()));
-                }
-            }
-
-        },newCalendar.get(Calendar.YEAR), newCalendar.get(Calendar.MONTH), newCalendar.get(Calendar.DAY_OF_MONTH));
-        datePickerDialog.show();
-    }
-    //End
 }
