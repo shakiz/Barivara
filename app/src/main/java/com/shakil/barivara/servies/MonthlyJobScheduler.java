ee18527ad801d6d327f33ce8e8143f27386eb23e
shakiz <activities335@gmail.com>
2023-04-02 00:28:36 +0600

    Add notification builder and intent for notification

--- app/src/main/java/com/shakil/barivara/servies/MonthlyJobScheduler.java
+++ app/src/main/java/com/shakil/barivara/servies/MonthlyJobScheduler.java
@@ -1,14 +1,22 @@
 package com.shakil.barivara.servies;
 
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.job.JobInfo;
 import android.app.job.JobScheduler;
 import android.content.BroadcastReceiver;
 import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.Build;
 
 import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+
+import com.shakil.barivara.R;
+import com.shakil.barivara.model.notification.Notification;
 
 public class MonthlyJobScheduler extends BroadcastReceiver {
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
@@ -19,5 +27,23 @@ public class MonthlyJobScheduler extends BroadcastReceiver {
                 .setPeriodic(30 * 24 * 60 * 60 * 1000L)
                 .build();
         jobScheduler.schedule(jobInfo);
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(
+                context,
+                1001,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE | PendingIntent.FLAG_UPDATE_CURRENT
+        );
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(context, "1001")
+                .setSmallIcon(R.drawable.ic_notifications_black_24dp)
+                .setContentTitle("")
+                .setContentText("")
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent);
+        notificationManager.notify(0, notification.build());
     }
 }
