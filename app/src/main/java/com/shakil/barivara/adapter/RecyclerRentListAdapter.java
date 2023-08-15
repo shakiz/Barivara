2f88954172027bd2daac2f5b98acaae598fe7fe3
shakiz <activities335@gmail.com>
2023-08-15 13:04:19 +0600

    Add view binding for rent bill list adapter

--- app/src/main/java/com/shakil/barivara/adapter/RecyclerRentListAdapter.java
+++ app/src/main/java/com/shakil/barivara/adapter/RecyclerRentListAdapter.java
@@ -3,19 +3,16 @@ package com.shakil.barivara.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.shakil.barivara.R;
+import com.shakil.barivara.databinding.AdapterLayoutRentListBinding;
 import com.shakil.barivara.model.room.Rent;
 
 import java.util.ArrayList;
 
-public class RecyclerRentListAdapter extends RecyclerView.Adapter<RecyclerRentListAdapter.ViewHolder> {
+public class RecyclerRentListAdapter extends RecyclerView.Adapter<RecyclerRentListAdapter.RentItemViewHolder> {
 
     private final ArrayList<Rent> arrayList;
 
@@ -31,39 +28,15 @@ public class RecyclerRentListAdapter extends RecyclerView.Adapter<RecyclerRentLi
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_layout_rent_list,parent,false);
-        return new ViewHolder(view);
+    public RentItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        AdapterLayoutRentListBinding binding = AdapterLayoutRentListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new RentItemViewHolder(binding, rentCallBacks);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull RentItemViewHolder holder, int position) {
         Rent rent = arrayList.get(position);
-        holder.MonthName.setText(rent.getMonthName());
-        holder.AssociateRoomName.setText(rent.getAssociateRoomName());
-        holder.RentAmount.setText(String.valueOf(rent.getRentAmount()));
-        holder.item_card_view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (rentCallBacks != null){
-                    rentCallBacks.onItemClick(rent);
-                }
-            }
-        });
-        holder.listCount.setText(""+(position+1));
-
-        holder.editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (rentCallBacks != null) rentCallBacks.onEdit(rent);
-            }
-        });
-        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (rentCallBacks != null) rentCallBacks.onDelete(rent);
-            }
-        });
+        holder.bind(rent);
     }
 
     @Override
@@ -71,28 +44,45 @@ public class RecyclerRentListAdapter extends RecyclerView.Adapter<RecyclerRentLi
         return arrayList.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView MonthName, AssociateRoomName, RentAmount;
-        CardView item_card_view;
-        TextView listCount;
-        Button editButton, deleteButton;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            MonthName = itemView.findViewById(R.id.MonthName);
-            AssociateRoomName = itemView.findViewById(R.id.AssociateRoomName);
-            RentAmount = itemView.findViewById(R.id.RentAmount);
-            listCount = itemView.findViewById(R.id.listCount);
-            editButton = itemView.findViewById(R.id.editButton);
-            deleteButton = itemView.findViewById(R.id.deleteButton);
-            item_card_view = itemView.findViewById(R.id.item_card_view);
+    public static class RentItemViewHolder extends RecyclerView.ViewHolder {
+        AdapterLayoutRentListBinding binding;
+        RentCallBacks rentCallBacks;
+        public RentItemViewHolder(@NonNull AdapterLayoutRentListBinding binding, RentCallBacks rentCallBacks) {
+            super(binding.getRoot());
+            this.binding = binding;
+            this.rentCallBacks = rentCallBacks;
+        }
+
+        void bind(Rent rent){
+            binding.MonthName.setText(rent.getMonthName());
+            binding.AssociateRoomName.setText(rent.getAssociateRoomName());
+            binding.RentAmount.setText(String.valueOf(rent.getRentAmount()));
+            binding.itemCardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (rentCallBacks != null){
+                        rentCallBacks.onItemClick(rent);
+                    }
+                }
+            });
+            binding.editDeleteIncludeLayout.editButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (rentCallBacks != null) rentCallBacks.onEdit(rent);
+                }
+            });
+            binding.editDeleteIncludeLayout.deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (rentCallBacks != null) rentCallBacks.onDelete(rent);
+                }
+            });
         }
     }
 
     public interface RentCallBacks {
         void onDelete(Rent rent);
-
         void onEdit(Rent rent);
-
         void onItemClick(Rent rent);
     }
 }
