da71e1e9f0355410046419cc332be1054f8390ac
shakiz <activities335@gmail.com>
2023-08-15 12:56:36 +0600

    Add view binding for meter bill list adapter

--- app/src/main/java/com/shakil/barivara/adapter/RecyclerMeterListAdapter.java
+++ app/src/main/java/com/shakil/barivara/adapter/RecyclerMeterListAdapter.java
@@ -3,20 +3,16 @@ package com.shakil.barivara.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.shakil.barivara.R;
+import com.shakil.barivara.databinding.AdapterLayoutMeterListBinding;
 import com.shakil.barivara.model.meter.Meter;
-import com.shakil.barivara.model.room.Rent;
 
 import java.util.ArrayList;
 
-public class RecyclerMeterListAdapter extends RecyclerView.Adapter<RecyclerMeterListAdapter.ViewHolder> {
+public class RecyclerMeterListAdapter extends RecyclerView.Adapter<RecyclerMeterListAdapter.MeterItemViewHolder> {
 
     private final ArrayList<Meter> arrayList;
 
@@ -32,39 +28,15 @@ public class RecyclerMeterListAdapter extends RecyclerView.Adapter<RecyclerMeter
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_layout_meter_list,parent,false);
-        return new ViewHolder(view);
+    public MeterItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        AdapterLayoutMeterListBinding binding = AdapterLayoutMeterListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new MeterItemViewHolder(binding, meterCallBacks);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull MeterItemViewHolder holder, int position) {
         Meter meter = arrayList.get(position);
-        holder.meterName.setText(meter.getMeterName());
-        holder.roomName.setText(meter.getAssociateRoom());
-        holder.meterType.setText(meter.getMeterTypeName());
-        holder.item_card_view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (meterCallBacks != null){
-                    meterCallBacks.onItemClick(meter);
-                }
-            }
-        });
-        holder.listCount.setText(""+(position+1));
-
-        holder.editButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (meterCallBacks != null) meterCallBacks.onEdit(meter);
-            }
-        });
-        holder.deleteButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (meterCallBacks != null) meterCallBacks.onDelete(meter);
-            }
-        });
+        holder.bind(meter);
     }
 
     @Override
@@ -72,20 +44,40 @@ public class RecyclerMeterListAdapter extends RecyclerView.Adapter<RecyclerMeter
         return arrayList.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView meterName, roomName, meterType;
-        CardView item_card_view;
-        TextView listCount;
-        Button editButton, deleteButton;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            meterName = itemView.findViewById(R.id.meterName);
-            roomName = itemView.findViewById(R.id.roomName);
-            meterType = itemView.findViewById(R.id.meterType);
-            listCount = itemView.findViewById(R.id.listCount);
-            editButton = itemView.findViewById(R.id.editButton);
-            deleteButton = itemView.findViewById(R.id.deleteButton);
-            item_card_view = itemView.findViewById(R.id.item_card_view);
+    public static class MeterItemViewHolder extends RecyclerView.ViewHolder {
+        AdapterLayoutMeterListBinding binding;
+        MeterCallBacks meterCallBacks;
+        public MeterItemViewHolder(@NonNull AdapterLayoutMeterListBinding binding, MeterCallBacks meterCallBacks) {
+            super(binding.getRoot());
+            this.binding = binding;
+            this.meterCallBacks = meterCallBacks;
+        }
+
+        void bind(Meter meter){
+            binding.meterName.setText(meter.getMeterName());
+            binding.roomName.setText(meter.getAssociateRoom());
+            binding.meterType.setText(meter.getMeterTypeName());
+            binding.itemCardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (meterCallBacks != null){
+                        meterCallBacks.onItemClick(meter);
+                    }
+                }
+            });
+
+            binding.editDeleteIncludeLayout.editButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (meterCallBacks != null) meterCallBacks.onEdit(meter);
+                }
+            });
+            binding.editDeleteIncludeLayout.deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (meterCallBacks != null) meterCallBacks.onDelete(meter);
+                }
+            });
         }
     }
 
