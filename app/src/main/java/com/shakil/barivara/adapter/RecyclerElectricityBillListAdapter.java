1e8279318c9b0ad2b3afc6cdd6040f9af7c9f453
shakiz <activities335@gmail.com>
2023-08-15 12:27:18 +0600

    Add view binding for electricity bill list adapter

--- app/src/main/java/com/shakil/barivara/adapter/RecyclerElectricityBillListAdapter.java
+++ app/src/main/java/com/shakil/barivara/adapter/RecyclerElectricityBillListAdapter.java
@@ -3,19 +3,16 @@ package com.shakil.barivara.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.cardview.widget.CardView;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.shakil.barivara.R;
+import com.shakil.barivara.databinding.AdapterLayoutElectricityBillListBinding;
 import com.shakil.barivara.model.meter.ElectricityBill;
 
 import java.util.ArrayList;
 
-public class RecyclerElectricityBillListAdapter extends RecyclerView.Adapter<RecyclerElectricityBillListAdapter.ViewHolder> {
+public class RecyclerElectricityBillListAdapter extends RecyclerView.Adapter<RecyclerElectricityBillListAdapter.ElectricityBillViewHolder> {
 
     private final ArrayList<ElectricityBill> arrayList;
 
@@ -31,34 +28,15 @@ public class RecyclerElectricityBillListAdapter extends RecyclerView.Adapter<Rec
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.adapter_layout_electricity_bill_list,parent,false);
-        return new ViewHolder(view);
+    public ElectricityBillViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        AdapterLayoutElectricityBillListBinding binding = AdapterLayoutElectricityBillListBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false);
+        return new ElectricityBillViewHolder(binding, electricityBillBacks);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ElectricityBillViewHolder holder, int position) {
         ElectricityBill electricityBill = arrayList.get(position);
-        holder.MeterId.setText(electricityBill.getMeterName());
-        holder.RoomId.setText(electricityBill.getRoomName());
-        holder.TotalBill.setText(String.valueOf(electricityBill.getTotalUnit() * electricityBill.getUnitPrice()));
-        holder.item_card_view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (electricityBillBacks != null){
-                    electricityBillBacks.onItemClick(electricityBill);
-                }
-            }
-        });
-
-        if (electricityBillBacks != null){
-            holder.DeleteFromCart.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    electricityBillBacks.onDelete(electricityBill);
-                }
-            });
-        }
+        holder.bind(electricityBill);
     }
 
     @Override
@@ -66,17 +44,36 @@ public class RecyclerElectricityBillListAdapter extends RecyclerView.Adapter<Rec
         return arrayList.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        TextView MeterId, RoomId, TotalBill;
-        CardView item_card_view;
-        ImageView DeleteFromCart;
-        public ViewHolder(@NonNull View itemView) {
-            super(itemView);
-            MeterId = itemView.findViewById(R.id.MeterId);
-            RoomId = itemView.findViewById(R.id.RoomId);
-            TotalBill = itemView.findViewById(R.id.TotalBill);
-            item_card_view = itemView.findViewById(R.id.item_card_view);
-            DeleteFromCart = itemView.findViewById(R.id.DeleteFromCart);
+    public static class ElectricityBillViewHolder extends RecyclerView.ViewHolder {
+        AdapterLayoutElectricityBillListBinding binding;
+        ElectricityBillBacks electricityBillBacks;
+        public ElectricityBillViewHolder(@NonNull AdapterLayoutElectricityBillListBinding binding, ElectricityBillBacks electricityBillBacks) {
+            super(binding.getRoot());
+            this.binding = binding;
+            this.electricityBillBacks = electricityBillBacks;
+        }
+
+        void bind(ElectricityBill electricityBill){
+            binding.MeterId.setText(electricityBill.getMeterName());
+            binding.RoomId.setText(electricityBill.getRoomName());
+            binding.TotalBill.setText(String.valueOf(electricityBill.getTotalUnit() * electricityBill.getUnitPrice()));
+            binding.itemCardView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (electricityBillBacks != null){
+                        electricityBillBacks.onItemClick(electricityBill);
+                    }
+                }
+            });
+
+            if (electricityBillBacks != null){
+                binding.DeleteFromCart.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        electricityBillBacks.onDelete(electricityBill);
+                    }
+                });
+            }
         }
     }
 
