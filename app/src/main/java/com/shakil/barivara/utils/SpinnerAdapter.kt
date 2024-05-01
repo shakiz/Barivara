4c17ed6f3474bb9a0b01f18a9149046d536cda4a
shakiz <activities335@gmail.com>
2024-05-01 15:28:23 +0600

    Kotlin Migration - SpinnerAdapter migrated

--- app/src/main/java/com/shakil/barivara/utils/SpinnerAdapter.kt
+++ app/src/main/java/com/shakil/barivara/utils/SpinnerAdapter.kt
@@ -1,18 +1,14 @@
-package com.shakil.barivara.utils;
+package com.shakil.barivara.utils
 
-import android.content.Context;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.content.Context
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import com.shakil.barivara.R
 
-import com.shakil.barivara.R;
-
-import java.util.ArrayList;
-
-public class SpinnerAdapter {
-
-    public void setSpinnerAdapter(Spinner spinner, Context context, ArrayList<String> spinnerValue){
-        ArrayAdapter<String> spinnerAdapter = new ArrayAdapter<>(context, R.layout.spinner_drop, spinnerValue);
-        spinnerAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item);
-        spinner.setAdapter(spinnerAdapter);
+class SpinnerAdapter {
+    fun setSpinnerAdapter(spinner: Spinner, context: Context?, spinnerValue: ArrayList<String>) {
+        val spinnerAdapter = ArrayAdapter(context!!, R.layout.spinner_drop, spinnerValue)
+        spinnerAdapter.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = spinnerAdapter
     }
 }
