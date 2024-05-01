fc788ee259c0529b9e65af387c1e58309bd397a2
shakiz <activities335@gmail.com>
2024-05-01 15:31:32 +0600

    Kotlin Migration - loadingView migrated

--- app/src/main/java/com/shakil/barivara/utils/UX.kt
+++ app/src/main/java/com/shakil/barivara/utils/UX.kt
@@ -1,27 +1,22 @@
-package com.shakil.barivara.utils;
+package com.shakil.barivara.utils
 
-import android.app.Dialog;
-import android.content.Context;
-import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
+import android.app.Dialog
+import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import com.shakil.barivara.R
 
-import com.shakil.barivara.R;
+class UX(context: Context) {
+    private val loadingDialog = Dialog(context)
 
-public class UX {
-    public Dialog loadingDialog;
-
-    public UX(Context context) {
-        loadingDialog = new Dialog(context);
+    fun getLoadingView() {
+        loadingDialog.setContentView(R.layout.loading_layout)
+        loadingDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        loadingDialog.setCanceledOnTouchOutside(false)
+        loadingDialog.show()
     }
 
-    public void getLoadingView(){
-        loadingDialog.setContentView(R.layout.loading_layout);
-        loadingDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-        loadingDialog.setCanceledOnTouchOutside(false);
-        loadingDialog.show();
-    }
-
-    public void removeLoadingView(){
-        if (loadingDialog.isShowing()) loadingDialog.cancel();
+    fun removeLoadingView() {
+        if (loadingDialog.isShowing) loadingDialog.cancel()
     }
 }
