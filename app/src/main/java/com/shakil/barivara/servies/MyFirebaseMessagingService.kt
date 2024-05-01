8576578d901fee927a7fa25cc4286da94d74a173
shakiz <activities335@gmail.com>
2024-05-01 20:27:41 +0600

    Kotlin Migration - Firebase messaging service migrated

--- app/src/main/java/com/shakil/barivara/servies/MyFirebaseMessagingService.kt
+++ app/src/main/java/com/shakil/barivara/servies/MyFirebaseMessagingService.kt
@@ -1,50 +1,41 @@
-package com.shakil.barivara.servies;
+package com.shakil.barivara.servies
 
-import static com.shakil.barivara.utils.Constants.TAG;
+import android.text.TextUtils
+import android.util.Log
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import com.shakil.barivara.firebasedb.FirebaseCrudHelper
+import com.shakil.barivara.model.notification.Notification
+import com.shakil.barivara.utils.Constants.TAG
+import com.shakil.barivara.utils.Tools.Companion.sendNotification
+import com.shakil.barivara.utils.UtilsForAll
+import java.util.UUID
 
-import android.text.TextUtils;
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.firebase.messaging.FirebaseMessagingService;
-import com.google.firebase.messaging.RemoteMessage;
-import com.shakil.barivara.firebasedb.FirebaseCrudHelper;
-import com.shakil.barivara.model.notification.Notification;
-import com.shakil.barivara.utils.Tools;
-import com.shakil.barivara.utils.UtilsForAll;
-
-import java.util.UUID;
-
-public class MyFirebaseMessagingService extends FirebaseMessagingService {
-
-    @Override
-    public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.i(TAG, "From: " + remoteMessage.getFrom());
-
-        Tools.sendNotification(this, remoteMessage.getNotification().getTitle(),
-                remoteMessage.getNotification().getBody());
-
-        FirebaseCrudHelper firebaseCrudHelper = new FirebaseCrudHelper(this);
-        UtilsForAll utilsForAll = new UtilsForAll(this);
-        if (
-                remoteMessage.getNotification().getTitle() != null &&
-                remoteMessage.getNotification().getBody() != null &&
-                !TextUtils.isEmpty(remoteMessage.getNotification().getTitle()) &&
-                !TextUtils.isEmpty(remoteMessage.getNotification().getBody())
-                ) {
-            Notification notification = new Notification(UUID.randomUUID().toString(),
-                    remoteMessage.getNotification().getTitle(),remoteMessage.getNotification().getBody(),
-                    utilsForAll.getDateTimeWithPM(),0);
-            firebaseCrudHelper.addNotification(notification, "notification");
-            Log.i(TAG,"Notification data saved");
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.i(TAG, "From: " + remoteMessage.from)
+        sendNotification(
+            this, remoteMessage.notification?.title,
+            remoteMessage.notification?.body
+        )
+        val firebaseCrudHelper = FirebaseCrudHelper(this)
+        val utilsForAll = UtilsForAll(this)
+        if (remoteMessage.notification?.title != null && remoteMessage.notification?.body != null &&
+            !TextUtils.isEmpty(remoteMessage.notification?.title) &&
+            !TextUtils.isEmpty(remoteMessage.notification?.body)
+        ) {
+            val notification = Notification(
+                UUID.randomUUID().toString(),
+                remoteMessage.notification?.title, remoteMessage.notification?.body,
+                utilsForAll.dateTimeWithPM, 0
+            )
+            firebaseCrudHelper.addNotification(notification, "notification")
+            Log.i(TAG, "Notification data saved")
         }
     }
 
-
-    @Override
-    public void onNewToken(@NonNull String s) {
-        Log.d("onNewToken:"+TAG,"New Token: "+s);
-        super.onNewToken(s);
+    override fun onNewToken(s: String) {
+        Log.d("onNewToken:$TAG", "New Token: $s")
+        super.onNewToken(s)
     }
 }
