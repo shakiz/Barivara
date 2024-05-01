12d74c31e55ecd16dfbc76eb3538caf058e892a1
shakiz <activities335@gmail.com>
2024-05-01 14:22:54 +0600

    Kotlin Migration - MyViewPagerAdapter migrated

--- app/src/main/java/com/shakil/barivara/utils/MyViewPagerAdapter.kt
+++ app/src/main/java/com/shakil/barivara/utils/MyViewPagerAdapter.kt
@@ -1,44 +1,31 @@
-package com.shakil.barivara.utils;
+package com.shakil.barivara.utils
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.viewpager.widget.PagerAdapter
 
-import androidx.viewpager.widget.PagerAdapter;
-
-public class MyViewPagerAdapter extends PagerAdapter {
-    private final Context context;
-    private final int[] layouts;
-
-    public MyViewPagerAdapter(Context context, int[] layouts) {
-        this.context = context;
-        this.layouts = layouts;
+class MyViewPagerAdapter(private val context: Context, private val layouts: IntArray) :
+    PagerAdapter() {
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val layoutInflater =
+            context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        val view = layoutInflater.inflate(layouts[position], container, false)
+        container.addView(view)
+        return view
     }
 
-    @Override
-    public Object instantiateItem(ViewGroup container, int position) {
-        LayoutInflater layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-
-        View view = layoutInflater.inflate(layouts[position], container, false);
-        container.addView(view);
-
-        return view;
+    override fun getCount(): Int {
+        return layouts.size
     }
 
-    @Override
-    public int getCount() {
-        return layouts.length;
+    override fun isViewFromObject(view: View, obj: Any): Boolean {
+        return view === obj
     }
 
-    @Override
-    public boolean isViewFromObject(View view, Object obj) {
-        return view == obj;
-    }
-
-    @Override
-    public void destroyItem(ViewGroup container, int position, Object object) {
-        View view = (View) object;
-        container.removeView(view);
+    override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
+        val view = `object` as View
+        container.removeView(view)
     }
 }
