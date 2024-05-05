e55eb37127a11d382c57654c23b636e279e74529
shakiz <activities335@gmail.com>
2024-05-05 12:26:40 +0600

    Kotlin Migration - Notification model migrated

--- app/src/main/java/com/shakil/barivara/model/notification/Notification.kt
+++ app/src/main/java/com/shakil/barivara/model/notification/Notification.kt
@@ -1,61 +1,15 @@
-package com.shakil.barivara.model.notification;
+package com.shakil.barivara.model.notification
 
-public class Notification {
-    private String NotificationId;
-    private String Title;
-    private String Message;
-    private String DateTime;
-    private int SeenStatus;
+class Notification(
+    NotificationId: String?,
+    Title: String?,
+    Message: String?,
+    DateTime: String?,
+    var seenStatus: Int
+) {
+    var notificationId: String? = NotificationId
+    var title: String? = Title
+    var message: String? = Message
+    var dateTime: String? = DateTime
 
-    public Notification(){
-        //Empty Constructor
-    }
-
-    public Notification(String NotificationId, String Title, String Message, String DateTime, int seenStatus) {
-        this.NotificationId = NotificationId;
-        this.Title = Title;
-        this.Message = Message;
-        this.DateTime = DateTime;
-        this.SeenStatus = seenStatus;
-    }
-
-    public String getNotificationId() {
-        return NotificationId;
-    }
-
-    public void setNotificationId(String notificationId) {
-        NotificationId = notificationId;
-    }
-
-    public String getTitle() {
-        return Title;
-    }
-
-    public void setTitle(String title) {
-        Title = title;
-    }
-
-    public String getMessage() {
-        return Message;
-    }
-
-    public void setMessage(String message) {
-        Message = message;
-    }
-
-    public String getDateTime() {
-        return DateTime;
-    }
-
-    public void setDateTime(String dateTime) {
-        DateTime = dateTime;
-    }
-
-    public int getSeenStatus() {
-        return SeenStatus;
-    }
-
-    public void setSeenStatus(int seenStatus) {
-        SeenStatus = seenStatus;
-    }
 }
