39c274da67d35d354a7fd29d2c852a02818240dd
shakiz <activities335@gmail.com>
2024-05-05 12:26:00 +0600

    Kotlin Migration - User model migrated

--- app/src/main/java/com/shakil/barivara/model/User.kt
+++ app/src/main/java/com/shakil/barivara/model/User.kt
@@ -1,98 +1,24 @@
-package com.shakil.barivara.model;
+package com.shakil.barivara.model
 
-import com.google.firebase.database.Exclude;
+import com.google.firebase.database.Exclude
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class User {
-    private String ID;
-    private String FirebaseKey;
-    private String Name;
-    private String UserName;
-    private String Mobile;
-    private String Email;
-    private String DOB;
-
-    public User(){
-
-    }
-
-    public User(String ID, String firebaseKey, String name, String userName, String mobile, String email, String DOB) {
-        this.ID = ID;
-        FirebaseKey = firebaseKey;
-        Name = name;
-        UserName = userName;
-        Mobile = mobile;
-        Email = email;
-        this.DOB = DOB;
-    }
-
-    public String getID() {
-        return ID;
-    }
-
-    public void setID(String ID) {
-        this.ID = ID;
-    }
-
-    public String getFirebaseKey() {
-        return FirebaseKey;
-    }
-
-    public void setFirebaseKey(String firebaseKey) {
-        FirebaseKey = firebaseKey;
-    }
-
-    public String getName() {
-        return Name;
-    }
-
-    public void setName(String name) {
-        Name = name;
-    }
-
-    public String getUserName() {
-        return UserName;
-    }
-
-    public void setUserName(String userName) {
-        UserName = userName;
-    }
-
-    public String getMobile() {
-        return Mobile;
-    }
-
-    public void setMobile(String mobile) {
-        Mobile = mobile;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public String getDOB() {
-        return DOB;
-    }
-
-    public void setDOB(String DOB) {
-        this.DOB = DOB;
-    }
-
+class User(
+    var id: String = "",
+    var firebaseKey: String = "",
+    var name: String = "",
+    var userName: String = "",
+    var mobile: String = "",
+    var email: String = "",
+    var dOB: String = ""
+) {
     @Exclude
-    public Map<String, Object> toMap() {
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("name", Name);
-        result.put("userName", UserName);
-        result.put("email", Email);
-        result.put("mobile", Mobile);
-        result.put("dOB", DOB);
-
-        return result;
+    fun toMap(): Map<String, Any> {
+        val result = HashMap<String, Any>()
+        result["name"] = name
+        result["userName"] = userName
+        result["email"] = email
+        result["mobile"] = mobile
+        result["dOB"] = dOB
+        return result
     }
 }
