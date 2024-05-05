8ac3318dc31e6c4271444ab12880e52ddb43b0dd
shakiz <activities335@gmail.com>
2024-05-05 12:26:57 +0600

    Kotlin Migration - Meter model migrated

--- app/src/main/java/com/shakil/barivara/model/meter/Meter.kt
+++ app/src/main/java/com/shakil/barivara/model/meter/Meter.kt
@@ -1,177 +1,98 @@
-package com.shakil.barivara.model.meter;
+package com.shakil.barivara.model.meter
 
-import android.os.Parcel;
-import android.os.Parcelable;
+import android.os.Parcel
+import android.os.Parcelable
+import com.google.firebase.database.Exclude
 
-import com.google.firebase.database.Exclude;
+class Meter : Parcelable {
+    var meterId: String = ""
+    var meterName: String = ""
+    var associateRoom: String = ""
+    var associateRoomId = 0
+    var meterTypeName: String = ""
+    var meterTypeId = 0
+    var presentUnit = 0
+    var pastUnit = 0
+    var fireBaseKey: String = ""
 
-import java.util.HashMap;
-import java.util.Map;
-
-public class Meter implements Parcelable {
-    private String MeterId;
-    private String MeterName;
-    private String AssociateRoom;
-    private int AssociateRoomId;
-    private String MeterTypeName;
-    private int MeterTypeId;
-    private int PresentUnit;
-    private int PastUnit;
-    private String FireBaseKey;
-
-    public Meter(String meterName, String associateRoom, String meterType, int presentUnit, int pastUnit) {
-        this.MeterName = meterName;
-        this.AssociateRoom = associateRoom;
-        this.MeterTypeName = meterType;
-        this.PresentUnit = presentUnit;
-        this.PastUnit = pastUnit;
+    constructor(
+        meterName: String,
+        associateRoom: String,
+        meterType: String,
+        presentUnit: Int,
+        pastUnit: Int
+    ) {
+        this.meterName = meterName
+        this.associateRoom = associateRoom
+        meterTypeName = meterType
+        this.presentUnit = presentUnit
+        this.pastUnit = pastUnit
     }
 
-    public Meter(String meterName, String associateRoom, String meterType) {
-        this.MeterName = meterName;
-        this.AssociateRoom = associateRoom;
-        this.MeterTypeName = meterType;
+    constructor(meterName: String, associateRoom: String, meterType: String) {
+        this.meterName = meterName
+        this.associateRoom = associateRoom
+        meterTypeName = meterType
     }
 
-    public Meter(String meterName, String owner, int presentUnit, int pastUnit) {
-        this.MeterName = meterName;
-        this.PresentUnit = presentUnit;
-        this.PastUnit = pastUnit;
+    constructor(meterName: String, owner: String, presentUnit: Int, pastUnit: Int) {
+        this.meterName = meterName
+        this.presentUnit = presentUnit
+        this.pastUnit = pastUnit
     }
 
-    public Meter() {
-
+    constructor()
+    constructor(meter: Parcel) {
+        meterId = meter.readString() ?: ""
+        meterName = meter.readString() ?: ""
+        associateRoom = meter.readString() ?: ""
+        associateRoomId = meter.readInt()
+        meterTypeName = meter.readString() ?: ""
+        meterTypeName = meter.readString() ?: ""
+        meterTypeId = meter.readInt()
+        presentUnit = meter.readInt()
+        pastUnit = meter.readInt()
+        fireBaseKey = meter.readString() ?: ""
     }
 
-    protected Meter(Parcel in) {
-        MeterId = in.readString();
-        MeterName = in.readString();
-        AssociateRoom = in.readString();
-        AssociateRoomId = in.readInt();
-        MeterTypeName = in.readString();
-        MeterTypeName = in.readString();
-        MeterTypeId = in.readInt();
-        PresentUnit = in.readInt();
-        PastUnit = in.readInt();
-        FireBaseKey = in.readString();
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(MeterId);
-        dest.writeString(MeterName);
-        dest.writeString(AssociateRoom);
-        dest.writeInt(AssociateRoomId);
-        dest.writeString(MeterTypeName);
-        dest.writeString(MeterTypeName);
-        dest.writeInt(MeterTypeId);
-        dest.writeInt(PresentUnit);
-        dest.writeInt(PastUnit);
-        dest.writeString(FireBaseKey);
-    }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    public static final Creator<Meter> CREATOR = new Creator<Meter>() {
-        @Override
-        public Meter createFromParcel(Parcel in) {
-            return new Meter(in);
-        }
-
-        @Override
-        public Meter[] newArray(int size) {
-            return new Meter[size];
-        }
-    };
-
-    public String getMeterId() {
-        return MeterId;
-    }
-
-    public void setMeterId(String meterId) {
-        MeterId = meterId;
-    }
-
-    public String getMeterName() {
-        return MeterName;
-    }
-
-    public void setMeterName(String meterName) {
-        MeterName = meterName;
-    }
-
-    public String getAssociateRoom() {
-        return AssociateRoom;
-    }
-
-    public void setAssociateRoom(String associateRoom) {
-        AssociateRoom = associateRoom;
-    }
-
-    public int getAssociateRoomId() {
-        return AssociateRoomId;
-    }
-
-    public void setAssociateRoomId(int associateRoomId) {
-        AssociateRoomId = associateRoomId;
-    }
-
-    public String getMeterTypeName() {
-        return MeterTypeName;
-    }
-
-    public void setMeterTypeName(String meterTypeName) {
-        MeterTypeName = meterTypeName;
-    }
-
-    public int getMeterTypeId() {
-        return MeterTypeId;
-    }
-
-    public void setMeterTypeId(int meterTypeId) {
-        MeterTypeId = meterTypeId;
-    }
-
-    public int getPresentUnit() {
-        return PresentUnit;
-    }
-
-    public void setPresentUnit(int presentUnit) {
-        PresentUnit = presentUnit;
-    }
-
-    public int getPastUnit() {
-        return PastUnit;
-    }
-
-    public void setPastUnit(int pastUnit) {
-        PastUnit = pastUnit;
-    }
-
-
-    public String getFireBaseKey() {
-        return FireBaseKey;
-    }
-
-    public void setFireBaseKey(String fireBaseKey) {
-        FireBaseKey = fireBaseKey;
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(meterId)
+        dest.writeString(meterName)
+        dest.writeString(associateRoom)
+        dest.writeInt(associateRoomId)
+        dest.writeString(meterTypeName)
+        dest.writeString(meterTypeName)
+        dest.writeInt(meterTypeId)
+        dest.writeInt(presentUnit)
+        dest.writeInt(pastUnit)
+        dest.writeString(fireBaseKey)
     }
 
     @Exclude
-    public Map<String, Object> toMap() {
-        HashMap<String, Object> result = new HashMap<>();
-        result.put("meterId", MeterId);
-        result.put("meterName", MeterName);
-        result.put("associateRoom", AssociateRoom);
-        result.put("associateRoomId", AssociateRoomId);
-        result.put("meterTypeName", MeterTypeName);
-        result.put("meterTypeId", MeterTypeId);
-        result.put("presentUnit", PresentUnit);
-        result.put("pastUnit", PastUnit);
+    fun toMap(): Map<String, Any> {
+        val result = HashMap<String, Any>()
+        result["meterId"] = meterId
+        result["meterName"] = meterName
+        result["associateRoom"] = associateRoom
+        result["associateRoomId"] = associateRoomId
+        result["meterTypeName"] = meterTypeName
+        result["meterTypeId"] = meterTypeId
+        result["presentUnit"] = presentUnit
+        result["pastUnit"] = pastUnit
+        return result
+    }
 
-        return result;
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Meter> {
+        override fun createFromParcel(parcel: Parcel): Meter {
+            return Meter(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Meter?> {
+            return arrayOfNulls(size)
+        }
     }
 }
