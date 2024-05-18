d0958c20e2f0aa250764d8fe01b70f8f922f5b42
shakiz <activities335@gmail.com>
2024-05-18 10:36:24 +0600

    Base Update - Update my app with firebase analytics

--- app/src/main/java/com/shakil/barivara/MyApplication.kt
+++ app/src/main/java/com/shakil/barivara/MyApplication.kt
@@ -1,8 +1,17 @@
 package com.shakil.barivara
 
 import android.app.Application
+import com.google.firebase.analytics.FirebaseAnalytics
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MyApplication : Application() {
+    companion object {
+        lateinit var firebaseAnalytics: FirebaseAnalytics
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        firebaseAnalytics = FirebaseAnalytics.getInstance(this)
+    }
 }
