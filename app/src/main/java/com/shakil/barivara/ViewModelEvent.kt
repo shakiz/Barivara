205b380a0f44bad87c4a478ed39659d287574499
shakiz <activities335@gmail.com>
2024-05-18 10:35:47 +0600

    Base Update - Add viewModelEvent extension

---
+++ app/src/main/java/com/shakil/barivara/ViewModelEvent.kt
@@ -0,0 +1,91 @@
+package com.shakil.barivara
+
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+
+/**
+ * Used as a wrapper for data that is exposed via a LiveData that represents an event.
+ */
+open class Event<out T>(private val content: T) {
+
+    var hasBeenHandled = false
+        private set // Allow external read but not write
+
+    /**
+     * Returns the content and prevents its use again.
+     */
+    fun getUnhandledContent(): T? {
+        return if (hasBeenHandled) {
+            null
+        } else {
+            hasBeenHandled = true
+            content
+        }
+    }
+
+    /**
+     * Returns the content, even if it's already been handled.
+     */
+    fun peekContent(): T = content
+}
+
+/**
+ * An [Observer] for [Event]s, simplifying the pattern of checking if the [Event]'s content has
+ * already been handled.
+ *
+ * [onUnhandledEvent] is *only* called if the [Event]'s contents has not been handled.
+ */
+class EventObserver<T>(private val onUnhandledEvent: (T) -> Unit) : Observer<Event<T>> {
+
+    override fun onChanged(value: Event<T>) {
+        value.getUnhandledContent()?.let { data ->
+            onUnhandledEvent(data)
+        }
+    }
+}
+
+
+/**
+ * A lifecycle-aware observable that sends only new updates after subscription,
+ * used for events like navigation and SnackBar messages.
+ * <p>
+ * This avoids a common problem with events: on configuration change (like rotation) an update
+ * can be emitted if the observer is active. This LiveData only calls the observable if there's an
+ * explicit call to setValue() or call().
+ * <p>
+ * Note that only one observer is going to be notified of changes.
+ */
+open class SingleLiveEvent<T> : MutableLiveData<T>() {
+
+    private val pending = AtomicBoolean(false)
+
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+
+        // Observe the internal MutableLiveData
+        super.observe(owner, { t ->
+            if (pending.compareAndSet(true, false)) {
+                observer.onChanged(t)
+            }
+        })
+    }
+
+    override fun postValue(value: T?) {
+        pending.set(true)
+        super.postValue(value)
+    }
+
+    @MainThread
+    override fun setValue(value: T?) {
+        pending.set(true)
+        super.setValue(value)
+    }
+
+    @MainThread
+    fun call() {
+        value = null
+    }
+}
