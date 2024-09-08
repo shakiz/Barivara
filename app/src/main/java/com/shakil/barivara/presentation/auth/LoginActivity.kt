a8e637393a3e4ab420eef2a1033ca16e3df561d0
shakiz <activities335@gmail.com>
2024-09-08 16:39:32 +0600

    New Auth Api - Add new auth send-otp implementation in login

--- app/src/main/java/com/shakil/barivara/presentation/auth/LoginActivity.kt
+++ app/src/main/java/com/shakil/barivara/presentation/auth/LoginActivity.kt
@@ -2,19 +2,14 @@ package com.shakil.barivara.presentation.auth
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import com.google.firebase.auth.FirebaseAuth
 import com.shakil.barivara.R
 import com.shakil.barivara.databinding.ActivityLoginBinding
-import com.shakil.barivara.presentation.onboard.MainActivity
-import com.shakil.barivara.utils.Constants
-import com.shakil.barivara.utils.PrefManager
-import com.shakil.barivara.utils.Tools
+import com.shakil.barivara.utils.Constants.mUserMobile
 import com.shakil.barivara.utils.UX
 import com.shakil.barivara.utils.UtilsForAll
 import com.shakil.barivara.utils.Validation
@@ -24,11 +19,9 @@ import es.dmoral.toasty.Toasty
 @AndroidEntryPoint
 class LoginActivity : AppCompatActivity() {
     private lateinit var activityBinding: ActivityLoginBinding
-    private var firebaseAuth: FirebaseAuth? = null
-    private var ux: UX? = null
-    private var utilsForAll: UtilsForAll? = null
+    private lateinit var ux: UX
+    private lateinit var utilsForAll: UtilsForAll
     private var loginWithStr: String? = null
-    private var tools = Tools(this)
     private val hashMap: Map<String?, Array<String>?> = HashMap()
     private var validation = Validation(this, hashMap)
 
@@ -36,7 +29,7 @@ class LoginActivity : AppCompatActivity() {
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
-            utilsForAll?.exitApp()
+            utilsForAll.exitApp()
         }
     }
 
@@ -53,50 +46,37 @@ class LoginActivity : AppCompatActivity() {
     private fun initUI() {
         ux = UX(this)
         utilsForAll = UtilsForAll(this)
-        firebaseAuth = FirebaseAuth.getInstance()
     }
 
     private fun initObservers() {
         viewModel.getSendOtpResponse().observe(this) { sendOtpResponse ->
             if (!sendOtpResponse.sendOtpResponse.otpValidationTime.isNullOrEmpty()) {
-                // Move to otp entry screen
-                Log.i("devShakil", "initObservers: $sendOtpResponse")
+                val intent = Intent(
+                    this, MobileRegVerificationActivity::class.java
+                )
+                intent.putExtra(mUserMobile, activityBinding.mobileNumber.text.toString())
+                startActivity(intent)
             }
         }
 
         viewModel.getSendOtpErrorResponse().observe(this) { sendOtpErrorResponse ->
-            Log.i("devShakil", "initObservers: $sendOtpErrorResponse")
+            Toasty.warning(this, getString(R.string.please_try_again_something_went_wrong)).show()
+        }
+
+        viewModel.isLoading.observe(this) { isLoading ->
+            if (isLoading) {
+                ux.getLoadingView()
+            } else {
+                ux.removeLoadingView()
+            }
         }
     }
 
     private fun bindUiWithComponents() {
         validation(arrayOf("mobileNumber"), arrayOf(getString(R.string.mobile_validation)))
-//        activityBinding.emailIdLayout.setOnClickListener {
-//            loginWithStr = getString(R.string.email)
-//            loginWith(loginWithStr)
-//            validation(
-//                arrayOf("email", "password"),
-//                arrayOf(
-//                    getString(R.string.email_validation),
-//                    getString(R.string.password_validation)
-//                )
-//            )
-//        }
-//        activityBinding.mobileLayout.setOnClickListener {
-//            loginWithStr = getString(R.string.mobile)
-//            loginWith(loginWithStr)
-//            validation(arrayOf("mobileNumber"), arrayOf(getString(R.string.mobile_validation)))
-//        }
-        activityBinding.register.setOnClickListener {
-            startActivity(
-                Intent(
-                    this@LoginActivity,
-                    RegistrationActivity::class.java
-                )
-            )
-        }
         activityBinding.login.setOnClickListener {
             if (!activityBinding.mobileNumber.text.isNullOrEmpty() && activityBinding.mobileNumber.text.length == 11) {
+                utilsForAll.hideSoftKeyboard(this)
                 viewModel.sendOtp(
                     activityBinding.mobileNumber.text.toString()
                 )
@@ -109,114 +89,6 @@ class LoginActivity : AppCompatActivity() {
                 ).show()
             }
         }
-        activityBinding.forgotPassword.setOnClickListener {
-            startActivity(
-                Intent(
-                    this@LoginActivity,
-                    ForgotPasswordActivity::class.java
-                )
-            )
-        }
-    }
-
-    private fun loginWithEmail() {
-        ux?.getLoadingView()
-        firebaseAuth?.signInWithEmailAndPassword(
-            activityBinding.email.text.toString(), activityBinding.password.text.toString()
-        )?.addOnCompleteListener { task ->
-            if (task.isSuccessful) {
-                Log.i(Constants.TAG + ":onComplete", getString(R.string.login_succcessful))
-                Toasty.success(
-                    this@LoginActivity,
-                    getString(R.string.login_succcessful),
-                    Toast.LENGTH_LONG,
-                    true
-                ).show()
-                tools.setLoginPrefs(task, PrefManager(this@LoginActivity))
-                startActivity(Intent(this@LoginActivity, MainActivity::class.java))
-            } else {
-                when (task.exception?.message) {
-                    getString(R.string.firebase_password_not_valid_exception) -> {
-                        Toasty.error(
-                            this@LoginActivity,
-                            getString(R.string.wrong_password),
-                            Toast.LENGTH_LONG,
-                            true
-                        ).show()
-                    }
-
-                    getString(R.string.firebase_no_user_exception) -> {
-                        Toasty.error(
-                            this@LoginActivity,
-                            getString(R.string.email_was_not_found_in_our_database),
-                            Toast.LENGTH_LONG,
-                            true
-                        ).show()
-                    }
-
-                    else -> {
-                        Toasty.error(
-                            this@LoginActivity,
-                            getString(R.string.login_unsucccessful),
-                            Toast.LENGTH_LONG,
-                            true
-                        ).show()
-                    }
-                }
-                Log.i(Constants.TAG + ":onComplete", getString(R.string.login_unsucccessful))
-            }
-            ux?.removeLoadingView()
-        }
-    }
-
-    private fun loginWithMobile() {
-        val intent = Intent(this@LoginActivity, MobileRegVerificationActivity::class.java)
-        intent.putExtra("mobile", activityBinding.mobileNumber.text.toString())
-        startActivity(intent)
-    }
-
-    private fun loginWith(registerWith: String?) {
-//        if (registerWith == getString(R.string.email)) {
-//            activityBinding.mainMobileRegistrationLayout.visibility = View.GONE
-//            activityBinding.mainEmailRegistrationLayout.visibility = View.VISIBLE
-//            activityBinding.emailIdLayout.background =
-//                ContextCompat.getDrawable(this, R.drawable.rectangle_background_filled_gender)
-//            activityBinding.EmailId.setTextColor(
-//                ContextCompat.getColor(
-//                    this,
-//                    R.color.md_white_1000
-//                )
-//            )
-//            activityBinding.mobileLayout.setBackgroundResource(0)
-//            activityBinding.Mobile.setTextColor(
-//                ContextCompat.getColor(
-//                    this,
-//                    R.color.md_green_800
-//                )
-//            )
-//            activityBinding.MobileIcon.setImageResource(R.drawable.ic_call_green)
-//            activityBinding.EmailIcon.setImageResource(R.drawable.ic_email_white)
-//        } else {
-//            activityBinding.mainEmailRegistrationLayout.visibility = View.GONE
-//            activityBinding.mainMobileRegistrationLayout.visibility = View.VISIBLE
-//            activityBinding.mobileLayout.background =
-//                ContextCompat.getDrawable(this, R.drawable.rectangle_background_filled_gender)
-//            activityBinding.emailIdLayout.setBackgroundResource(0)
-//            activityBinding.EmailId.setTextColor(
-//                ContextCompat.getColor(
-//                    this,
-//                    R.color.md_green_800
-//                )
-//            )
-//            activityBinding.Mobile.setTextColor(
-//                ContextCompat.getColor(
-//                    this,
-//                    R.color.md_white_1000
-//                )
-//            )
-//            activityBinding.MobileIcon.setImageResource(R.drawable.ic_call_white)
-//            activityBinding.EmailIcon.setImageResource(R.drawable.ic_email_green)
-//        }
     }
 
     private fun validation(resNames: Array<String>, validationMessages: Array<String>) {
