77ace58fa188998fa41d5416daef3402b30ad871
shakiz <activities335@gmail.com>
2024-09-16 13:35:13 +0600

    Add room service

---
+++ app/src/main/java/com/shakil/barivara/data/remote/webservice/RoomService.kt
@@ -0,0 +1,26 @@
+package com.shakil.barivara.data.remote.webservice
+
+import com.shakil.barivara.data.model.BaseApiResponse
+import com.shakil.barivara.data.model.tenant.Tenant
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
+import retrofit2.http.POST
+
+interface RoomService {
+    @GET("rooms")
+    suspend fun getAllTenant(
+        @Header("Authorization") token: String,
+        @Header("Content-Type") contentType: String,
+        @Header("Accept") accept: String,
+    ): Response<List<Tenant>>
+
+    @POST("rooms")
+    suspend fun addTenant(
+        @Header("Authorization") token: String,
+        @Header("Content-Type") contentType: String,
+        @Header("Accept") accept: String,
+        @Body tenant: Tenant
+    ): Response<BaseApiResponse>
+}
