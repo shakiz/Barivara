6f35430ccfe76654455e1c5bbf0a54c2d4527f19
shakiz <activities335@gmail.com>
2024-10-19 12:05:37 +0600

    Update login response

--- app/src/main/java/com/shakil/barivara/data/model/auth/VerifyOtpResponse.kt
+++ app/src/main/java/com/shakil/barivara/data/model/auth/VerifyOtpResponse.kt
@@ -2,13 +2,16 @@ package com.shakil.barivara.data.model.auth
 
 import com.google.gson.annotations.SerializedName
 import com.shakil.barivara.data.model.BaseApiResponse
+import com.shakil.barivara.data.model.user.UserInfo
 
 class VerifyOtpBaseResponse(@SerializedName("data") val verifyOtpResponse: VerifyOtpResponse) :
     BaseApiResponse()
 
 data class VerifyOtpResponse(
     @SerializedName("access_token") var accessToken: String? = null,
-    @SerializedName("user_id") var userId: Int,
     @SerializedName("token_type") var tokenType: String? = null,
-    @SerializedName("expires_in") var expiresIn: Int? = null
+    @SerializedName("expires_at") var expiresAt: Int? = null,
+    @SerializedName("refresh_token") var refreshToken: String? = null,
+    @SerializedName("refresh_token_expires_at") var refreshTokenExpiresAt: Int? = null,
+    @SerializedName("profile") var userInfo: UserInfo
 )
