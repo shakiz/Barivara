1d28978ca946edba47e89bdb805b71d43797c0c7
shakiz <activities335@gmail.com>
2024-11-03 20:54:03 +0600

    Add auto image slider in home UI

--- app/src/main/java/com/shakil/barivara/presentation/onboard/HomeActivity.kt
+++ app/src/main/java/com/shakil/barivara/presentation/onboard/HomeActivity.kt
@@ -5,19 +5,22 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
 import android.view.WindowManager
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.OnBackPressedCallback
 import androidx.activity.viewModels
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.LinearSnapHelper
+import androidx.viewpager2.widget.ViewPager2
 import com.google.android.material.navigation.NavigationView
 import com.google.firebase.FirebaseApp
 import com.google.firebase.messaging.FirebaseMessaging
@@ -53,6 +56,9 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(),
     private var tools = Tools(this)
     private lateinit var ux: UX
     private val viewModel by viewModels<HomeViewModel>()
+    private var currentPage = 0
+    private val slideDelay: Long = 3000
+    private val handler = Handler(Looper.getMainLooper())
 
     private val onBackPressedCallback = object : OnBackPressedCallback(true) {
         override fun handleOnBackPressed() {
@@ -70,61 +76,11 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(),
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         screenViewed(ScreenNameConstants.appSreenHome)
-
-        // Set up the adapter
-        val adapter = ImageSliderAdapter(this, viewModel.getSliderData())
-        val snapHelper = LinearSnapHelper()
-        snapHelper.attachToRecyclerView(activityMainBinding.imageSlider)
-        activityMainBinding.imageSlider.adapter = adapter
-        activityMainBinding.imageSlider.layoutManager =
-            LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
         init()
-        activityMainBinding.toolBar.setTitleTextColor(
-            ContextCompat.getColor(
-                this,
-                R.color.md_green_800
-            )
-        )
+        setUpSlider()
         setupDrawerToggle()
-        FirebaseApp.initializeApp(this)
-        FirebaseMessaging.getInstance().subscribeToTopic("general")
-            .addOnCompleteListener { task ->
-                if (task.isSuccessful) {
-                    Log.i(Constants.TAG, "Successfully received notification")
-                } else {
-                    Log.i(Constants.TAG, "Notification received failed")
-                }
-            }
+        setupNotification()
         LanguageManager(this).configLanguage()
-        if (Build.VERSION.SDK_INT > 32) {
-            if (ContextCompat.checkSelfPermission(
-                    this@HomeActivity,
-                    Manifest.permission.CALL_PHONE
-                ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                    this@HomeActivity,
-                    Manifest.permission.POST_NOTIFICATIONS
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this@HomeActivity,
-                    arrayOf(Manifest.permission.CALL_PHONE, Manifest.permission.POST_NOTIFICATIONS),
-                    Constants.REQUEST_CALL_CODE
-                )
-            }
-        } else {
-            if (ContextCompat.checkSelfPermission(
-                    this@HomeActivity,
-                    Manifest.permission.CALL_PHONE
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                ActivityCompat.requestPermissions(
-                    this@HomeActivity,
-                    arrayOf(Manifest.permission.CALL_PHONE),
-                    Constants.REQUEST_CALL_CODE
-                )
-            }
-        }
         bindUIWithComponents()
         initListeners()
         initObservers()
@@ -135,6 +91,91 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(),
     private fun init() {
         prefManager = PrefManager(this)
         ux = UX(this)
+    }
+
+    private fun setUpSlider() {
+        // Set up the adapter
+        val adapter = ImageSliderAdapter(this, viewModel.getSliderData())
+        val loopingImageList = viewModel.getSliderData() + viewModel.getSliderData()[0]
+        activityMainBinding.viewPager.adapter = adapter
+        // Create indicators
+        setupIndicators(viewModel.getSliderData().size)
+        setCurrentIndicator(0)
+
+        // Change indicator on page change
+        activityMainBinding.viewPager.registerOnPageChangeCallback(object :
+            ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                if (position == loopingImageList.size - 1) {
+                    // If at the fake last position, reset to the real first position without animation
+                    handler.postDelayed({
+                        activityMainBinding.viewPager.setCurrentItem(
+                            0,
+                            false
+                        ) // false disables animation
+                    }, 300) // Short delay to allow smooth transition
+                } else {
+                    setCurrentIndicator(position % viewModel.getSliderData().size)
+                    currentPage = position
+                }
+            }
+        })
+
+        // Start auto-slide
+        startAutoSlide()
+    }
+
+    private fun setupIndicators(count: Int) {
+        val indicators = Array(count) { ImageView(this) }
+        val params = LinearLayout.LayoutParams(
+            24,
+            24
+        ).apply {
+            setMargins(8, 0, 8, 0)
+        }
+
+        for (i in indicators.indices) {
+            indicators[i] = ImageView(this).apply {
+                setImageResource(R.drawable.ic_circle)
+                layoutParams = params
+            }
+            activityMainBinding.indicatorLayout.addView(indicators[i])
+        }
+    }
+
+    private fun setCurrentIndicator(index: Int) {
+        val childCount = activityMainBinding.indicatorLayout.childCount
+        for (i in 0 until childCount) {
+            val imageView = activityMainBinding.indicatorLayout.getChildAt(i) as ImageView
+            imageView.setImageResource(
+                if (i == index) R.drawable.ic_circle else R.drawable.ic_circle_inactive
+            )
+        }
+    }
+
+    private fun startAutoSlide() {
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                if (currentPage == viewModel.getSliderData().size) {
+                    currentPage = 0
+                }
+                activityMainBinding.viewPager.setCurrentItem(currentPage++, true)
+                handler.postDelayed(this, slideDelay)
+            }
+        }, slideDelay)
+    }
+
+    private fun setupNotification() {
+        FirebaseApp.initializeApp(this)
+        FirebaseMessaging.getInstance().subscribeToTopic("general")
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Log.i(Constants.TAG, "Successfully received notification")
+                } else {
+                    Log.i(Constants.TAG, "Notification received failed")
+                }
+            }
     }
 
     private fun setupDrawerToggle() {
@@ -156,6 +197,13 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(),
     }
 
     private fun initListeners() {
+        activityMainBinding.toolBar.setTitleTextColor(
+            ContextCompat.getColor(
+                this,
+                R.color.md_green_800
+            )
+        )
+
         onBackPressedDispatcher.addCallback(this, onBackPressedCallback)
 
         activityMainBinding.totalRoomFlatLayout.setOnClickListener {
@@ -249,10 +297,35 @@ class HomeActivity : BaseActivity<ActivityHomeBinding>(),
 
     private fun bindUIWithComponents() {
         activityMainBinding.navigationView.setNavigationItemSelectedListener(this)
-        if (prefManager.getInt(Constants.mAppViewCount) > 0) {
-            prefManager[Constants.mAppViewCount] =
-                prefManager.getInt(Constants.mAppViewCount) + 1
-        } else prefManager[Constants.mAppViewCount] = 1
+
+        if (Build.VERSION.SDK_INT > 32) {
+            if (ContextCompat.checkSelfPermission(
+                    this@HomeActivity,
+                    Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
+                    this@HomeActivity,
+                    Manifest.permission.POST_NOTIFICATIONS
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@HomeActivity,
+                    arrayOf(Manifest.permission.CALL_PHONE, Manifest.permission.POST_NOTIFICATIONS),
+                    Constants.REQUEST_CALL_CODE
+                )
+            }
+        } else {
+            if (ContextCompat.checkSelfPermission(
+                    this@HomeActivity,
+                    Manifest.permission.CALL_PHONE
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    this@HomeActivity,
+                    arrayOf(Manifest.permission.CALL_PHONE),
+                    Constants.REQUEST_CALL_CODE
+                )
+            }
+        }
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
