0cedf2e97c8f8d54faec92bc21690efc0f98e4c9
shakiz <activities335@gmail.com>
2025-01-05 00:06:14 +0600

    feature(bill-history) - Populate tenant spinner

--- app/src/main/java/com/shakil/barivara/presentation/generatebill/GenerateBillViewModel.kt
+++ app/src/main/java/com/shakil/barivara/presentation/generatebill/GenerateBillViewModel.kt
@@ -7,7 +7,9 @@ import androidx.lifecycle.viewModelScope
 import com.shakil.barivara.data.model.BaseApiResponse
 import com.shakil.barivara.data.model.generatebill.BillInfo
 import com.shakil.barivara.data.model.generatebill.GenerateBillResponse
+import com.shakil.barivara.data.model.tenant.Tenant
 import com.shakil.barivara.data.repository.GenerateBillRepoImpl
+import com.shakil.barivara.data.repository.TenantRepoImpl
 import com.shakil.barivara.utils.ErrorType
 import com.shakil.barivara.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -15,7 +17,10 @@ import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class GenerateBillViewModel @Inject constructor(private val generalBillRepoImpl: GenerateBillRepoImpl) :
+class GenerateBillViewModel @Inject constructor(
+    private val generalBillRepoImpl: GenerateBillRepoImpl,
+    private val tenantRepoImpl: TenantRepoImpl
+) :
     ViewModel() {
     var isLoading = MutableLiveData<Boolean>()
 
@@ -25,6 +30,9 @@ class GenerateBillViewModel @Inject constructor(private val generalBillRepoImpl:
 
     private var updateRentStatusResponse = MutableLiveData<BaseApiResponse>()
     private var updateRentStatusErrorResponse = MutableLiveData<Resource.Error<ErrorType>>()
+
+    private var tenants = MutableLiveData<List<Tenant>>()
+    private var getTenantListErrorResponse = MutableLiveData<Resource.Error<ErrorType>>()
 
     fun getBills(): LiveData<List<BillInfo>> {
         return bills
@@ -44,6 +52,10 @@ class GenerateBillViewModel @Inject constructor(private val generalBillRepoImpl:
 
     fun getUpdateRentStatusErrorResponse(): LiveData<Resource.Error<ErrorType>> {
         return updateRentStatusErrorResponse
+    }
+
+    fun getTenants(): LiveData<List<Tenant>> {
+        return tenants
     }
 
     fun generateBill(year: Int, month: Int) {
@@ -95,6 +107,34 @@ class GenerateBillViewModel @Inject constructor(private val generalBillRepoImpl:
                 isLoading.postValue(false)
             } catch (e: Exception) {
                 updateRentStatusErrorResponse.postValue(
+                    Resource.Error(
+                        message = "Something went wrong, please try again",
+                        errorType = ErrorType.UNKNOWN
+                    )
+                )
+                isLoading.postValue(false)
+            }
+        }
+    }
+
+    fun getAllTenants() {
+        viewModelScope.launch {
+            isLoading.postValue(true)
+            try {
+                val data = tenantRepoImpl.getAllTenant()
+                if (data.response != null) {
+                    tenants.postValue(data.response)
+                } else {
+                    getTenantListErrorResponse.postValue(
+                        Resource.Error(
+                            message = data.message,
+                            errorType = data.errorType
+                        )
+                    )
+                }
+                isLoading.postValue(false)
+            } catch (e: Exception) {
+                getTenantListErrorResponse.postValue(
                     Resource.Error(
                         message = "Something went wrong, please try again",
                         errorType = ErrorType.UNKNOWN
--- app/src/main/java/com/shakil/barivara/presentation/generatebill/GeneratedBillHistoryActivity.kt
+++ app/src/main/java/com/shakil/barivara/presentation/generatebill/GeneratedBillHistoryActivity.kt
@@ -1,19 +1,21 @@
 package com.shakil.barivara.presentation.generatebill
 
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import androidx.activity.viewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.shakil.barivara.BaseActivity
 import com.shakil.barivara.R
 import com.shakil.barivara.databinding.ActivityGeneratedBillHistoryBinding
 import com.shakil.barivara.presentation.adapter.RecyclerBillInfoAdapter
+import com.shakil.barivara.utils.DatePickerManager
 import com.shakil.barivara.utils.PrefManager
 import com.shakil.barivara.utils.ScreenNameConstants
 import com.shakil.barivara.utils.SpinnerAdapter
 import com.shakil.barivara.utils.SpinnerData
 import com.shakil.barivara.utils.UX
 import com.shakil.barivara.utils.UtilsForAll
-import com.shakil.barivara.utils.Validation
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -23,18 +25,18 @@ class GeneratedBillHistoryActivity : BaseActivity<ActivityGeneratedBillHistoryBi
     private val hashMap: Map<String?, Array<String>?> = HashMap()
     private var year: Int = 0
     private var month: Int = 0
-    private var validation = Validation(this, hashMap)
+    private var tenantId: Int = 0
     private var spinnerData = SpinnerData(this)
     private var spinnerAdapter = SpinnerAdapter()
+
     @Inject
     lateinit var utilsForAll: UtilsForAll
     private lateinit var ux: UX
+    private val viewModel by viewModels<GenerateBillViewModel>()
 
     @Inject
     lateinit var prefManager: PrefManager
     private lateinit var recyclerBillInfoAdapter: RecyclerBillInfoAdapter
-
-    private val viewModel by viewModels<GenerateBillViewModel>()
 
     override val layoutResourceId: Int
         get() = R.layout.activity_generated_bill_history
@@ -48,8 +50,10 @@ class GeneratedBillHistoryActivity : BaseActivity<ActivityGeneratedBillHistoryBi
         screenViewed(ScreenNameConstants.appSreenGenerateBill)
         init()
         binUIWithComponents()
+        initListeners()
         initObservers()
         setRecyclerAdapter()
+        viewModel.getAllTenants()
     }
 
     private fun init() {
@@ -64,10 +68,57 @@ class GeneratedBillHistoryActivity : BaseActivity<ActivityGeneratedBillHistoryBi
                 ux.removeLoadingView()
             }
         }
+
+        viewModel.getTenants().observe(this) { tenants ->
+            if (tenants.isEmpty()) {
+                spinnerAdapter.setSpinnerAdapter(
+                    activityBinding.searchLayout.tenantNameId,
+                    this,
+                    spinnerData.setSpinnerNoData()
+                )
+            } else {
+                val tenantList = mutableListOf<String>()
+                tenantList.add(0, getString(R.string.select_data_1))
+                tenantList.addAll(ArrayList(tenants.map { it.name }))
+                spinnerAdapter.setSpinnerAdapter(
+                    activityBinding.searchLayout.tenantNameId,
+                    this,
+                    ArrayList(tenantList)
+                )
+            }
+        }
     }
 
     private fun binUIWithComponents() {
         activityBinding.toolBar.setNavigationOnClickListener { finish() }
+    }
+
+    private fun initListeners() {
+        activityBinding.searchLayout.startingMonthId.setOnClickListener {
+            DatePickerManager().showDatePicker(this, object : DatePickerManager.DatePickerCallback {
+                override fun onDateSelected(date: String) {
+                    activityBinding.searchLayout.startingMonthId.setText(date)
+                }
+            })
+        }
+
+        activityBinding.searchLayout.tenantNameId.onItemSelectedListener =
+            object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View,
+                    position: Int,
+                    id: Long
+                ) {
+                    if (!viewModel.getTenants().value.isNullOrEmpty()) {
+                        viewModel.getTenants().value?.let {
+                            tenantId = it[position].id
+                        }
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>?) {}
+            }
     }
 
     private fun setRecyclerAdapter() {
