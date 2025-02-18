f4d89684f73932455f6c5ed3873c05af35b3cc1d
shakiz <activities335@gmail.com>
2025-02-18 23:22:29 +0600

    feature(payment-received-date)- Add payment received date implementation in bill history

--- app/src/main/java/com/shakil/barivara/utils/ExtensionManager.kt
+++ app/src/main/java/com/shakil/barivara/utils/ExtensionManager.kt
@@ -4,6 +4,11 @@ import android.graphics.Typeface
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.StyleSpan
+import com.shakil.barivara.utils.DateTimeConstants.API_DATETIME_FORMAT_Z
+import com.shakil.barivara.utils.DateTimeConstants.APP_DATE_FORMAT
+import java.text.SimpleDateFormat
+import java.util.Locale
+import java.util.TimeZone
 
 fun String.boldAfterColon(): SpannableString {
     val colonIndex = this.indexOf(":")
@@ -23,5 +28,28 @@ fun String.boldAfterColon(): SpannableString {
         spannableString
     } else {
         SpannableString(this) // Return the original string if no colon is found
+    }
+}
+
+fun String.formatDateIntoAppDate(): String {
+    val inputFormat = SimpleDateFormat(API_DATETIME_FORMAT_Z, Locale.ENGLISH)
+    inputFormat.timeZone = TimeZone.getTimeZone("UTC") // Ensure UTC timezone
+
+    val outputFormat = SimpleDateFormat(APP_DATE_FORMAT, Locale.ENGLISH)
+
+    val date = inputFormat.parse(this) ?: return this
+    val day = SimpleDateFormat("d", Locale.ENGLISH).format(date).toInt()
+    val suffix = getDaySuffix(day)
+
+    return "${day}$suffix ${outputFormat.format(date).substring(2)}"
+}
+
+fun getDaySuffix(day: Int): String {
+    return when {
+        day in 11..13 -> "th"
+        day % 10 == 1 -> "st"
+        day % 10 == 2 -> "nd"
+        day % 10 == 3 -> "rd"
+        else -> "th"
     }
 }
